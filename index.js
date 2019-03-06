b930a2c8ab14b5bda0b4034e7650f718168fd450
RaghebBenA <ragheb.pr.2014@gmail.com>
2019-03-06 21:47:27 +0100

    node callbacks and error handeling

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const rect = require('./rectangle')
 
-const solveRect = (l,b) =>{
-console.log("Solving for rectangle with l = " + l + " and b =" +b)
- if (l <= 0 || b <=0){
-     console.log("Rectangle dimensions should be greater than zero")
- }else{
-     console.log("the area of the rectanle is" + rect.area(l,b))
-     console.log('the permiter of the rectangle is' + rect.permiter(l,b))
- }
+const solveRect = (l, b) => {
+    console.log("Solving for rectangle with l = " + l + " and b =" + b)
+
+    rect(l, b, (err, rectangle) => {
+        if (err) {
+            console.log("EROOR", err.message)
+        }
+        else {
+            console.log('the area of the rectangle of dimensions l = ' + l + " and b = " + b + ' is ' + rectangle.area())
+            console.log('the permiter of the rectangle of dimensions l = ' + l + " and b = " + b + ' is ' + rectangle.permiter())
+        }
+    })
+    console.log('this statment is after the call to rect()')
 }
 
-solveRect(4,8)
-solveRect(5,8)
-solveRect(0,4)
+solveRect(4, 8)
+solveRect(5, 8)
+solveRect(0, 4)
--- rectangle.js
+++ rectangle.js
@@ -1,2 +1,20 @@
-exports.permiter = (x,y) => (2*(x+y))
-exports.area = (x,y) =>(x*y)
+module.exports = (x, y, callback) => {
+
+    if (x <= 0 || y <= 0) {
+        setTimeout(() =>
+            callback(new Error("Rectangle dimensions should be greater than zero"), null),
+            2000)
+
+    } else {
+        setTimeout(() =>
+            callback(null,
+                {permiter :() => (2 * (x + y)),
+                    area : () => (x * y)}),
+            2000)
+    }
+}
+
+
+
+
+
