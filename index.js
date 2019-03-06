d5fc3164baa62bd42036f7429a76d600f52ff413
RaghebBenA <ragheb.pr.2014@gmail.com>
2019-03-06 15:50:22 +0100

    simple node example

---
+++ index.js
@@ -0,0 +1,17 @@
+const rect = {
+    permiter: (x,y) => (2*(x+y)),
+    area: (x,y) =>(x*y)
+}
+const solveRect = (l,b) =>{
+console.log("Solving for rectangle with l = " + l + "and b =" +b)
+ if (l <= 0 || b <=0){
+     console.log("Rectangle dimensions should be greater than zero")
+ }else{
+     console.log("the area of the rectanle is" + rect.area(l,b))
+     console.log('the permiter of the rectangle is' + rect.permiter(l,b))
+ }
+}
+
+solveRect(4,8)
+solveRect(5,8)
+solveRect(0,4)
